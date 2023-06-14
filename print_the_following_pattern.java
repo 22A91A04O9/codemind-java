b71f202f3bc0dad497fc67d3e605102e00508234
22A91A04O9 <119720621+22A91A04O9@users.noreply.github.com>
2023-06-14 23:38:23 +0530

    import java.util.*;  public class praveen{ public static void main(String args[]){     int a;     Scanner s=new Scanner(System.in);     a=s.nextInt();     for(int i=a;i>=1;i--)     {         for(int j=1;j<=i;j++)         {             System.out.format("%c ",i+64);
            }
            System.out.println();
        }
    }
    }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -4,11 +4,11 @@ public static void main(String args[]){
     int a;
     Scanner s=new Scanner(System.in);
     a=s.nextInt();
-    for(int i=1;i<=a;i++)
+    for(int i=a;i>=1;i--)
     {
         for(int j=1;j<=i;j++)
         {
-            System.out.format("%d",j);
+            System.out.format("%c ",i+64);
             
         }
         System.out.println();
