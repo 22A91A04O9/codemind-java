03009edd1fa01e3be7d94e9be78fafdc39bf3143
22A91A04O9 <119720621+22A91A04O9@users.noreply.github.com>
2025-02-28 21:47:29 +0530

    Add files via upload

---
+++ typecast.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String num = sc.nextLine();
+        int intValue = Integer.parseInt(num); 
+        System.out.println(intValue * 2); 
+        sc.close();
+    }
+}
