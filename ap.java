0b6c5be5e01698d3edc3fa58d41855c32cc78425
22A91A04O9 <119720621+22A91A04O9@users.noreply.github.com>
2025-03-01 23:07:09 +0530

    Add files via upload

---
+++ ap.java
@@ -0,0 +1,8 @@
+public static int apTerm(int a, int d, int n) {
+    // Complete the code above
+    int ans =a + (n-1)*d;
+        // Complete the code above
+
+        // The line below returns the output. Don't change it!
+        return ans;
+}
