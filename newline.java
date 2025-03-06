1a2cecdce5224f7d1dfd42f51f5dbefade6595f8
22A91A04O9 <119720621+22A91A04O9@users.noreply.github.com>
2025-03-06 22:13:23 +0530

    Add files via upload

---
+++ newline.java
@@ -0,0 +1,14 @@
+class Geeks {
+    public void printIndividualLine() {
+        System.out.println("Geeks");
+        System.out.println("for");
+        System.out.println("Geeks");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Geeks g = new Geeks(); 
+        g.printIndividualLine(); 
+    }
+}
